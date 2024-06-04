079266ceb1f50ad2356a1423d16fccc217f687ef
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-06-04 17:18:57 +0300

    удалить комменты

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,12 +2,6 @@ import path from "path";
 import { buildWebpack } from "./config/build/buildWebpack";
 import { EnvVariables } from "./config/build/types/types";
 
-// для добавления стилей необходимо установить не только css лоудер, но и style лоудер
-
-// path.resolve(__dirname, "src", "index.tsx"),
-//
-//
-
 export default (env: EnvVariables) =>
   buildWebpack({
     port: env.port ?? 3000,
